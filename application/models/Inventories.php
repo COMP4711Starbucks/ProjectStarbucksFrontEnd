6d5bab85015f4679f20383dd7c5b91049db5adc4
Lei Zhang(Daniel) <danielvmaple@gmail.com>
2016-12-11 22:12:53 -0800

    Fixed RESTful issue

--- application/models/Inventories.php
+++ application/models/Inventories.php
@@ -5,13 +5,13 @@
  */
 class Inventories extends CI_Model {
     // Constructor
-    public function __construct()
+    function __construct()
     {
         parent::__construct();
         $this->load->library(['curl', 'format', 'rest']);
     }
 
-    public function rules() { 
+    function rules() { 
         $config = [
             ['field'=>'name', 'label'=>'Inventory name', 'rules'=> 'required'], 
             ['field'=>'quantity', 'label'=>'Inventory quantity', 'rules'=> 'required|integer']
@@ -20,7 +20,7 @@ class Inventories extends CI_Model {
     }
     
     // retrieve recipes by passing inventory id
-    public function getMenu($id)
+    function getMenu($id)
     {
         $recipes = $this->recipes->all();
         $menus = $this->menu->all();
@@ -46,7 +46,7 @@ class Inventories extends CI_Model {
     }
     
     // retrieve one inventory name by passing id
-    public function getName($id){
+    function getName($id){
         $result = $this->all();
         // iterate over the data until we find the one we want
         foreach ($result as $record){
@@ -62,7 +62,7 @@ class Inventories extends CI_Model {
     {
             $this->rest->initialize(array('server' => REST_SERVER));
             $this->rest->option(CURLOPT_PORT, REST_PORT);
-            return $this->rest->get('/inventory/maintenance');
+            return $this->rest->get('inventory/maintenance');
     }
     
     // Retrieve an existing DB record as an object
@@ -70,7 +70,7 @@ class Inventories extends CI_Model {
     {
             $this->rest->initialize(array('server' => REST_SERVER));
             $this->rest->option(CURLOPT_PORT, REST_PORT);
-            return $this->rest->get('/maintenance/item/id/' . $key);
+            return $this->rest->get('inventory/maintenance/item/id/' . $key);
     }
     
     // Create a new data object.
@@ -78,7 +78,7 @@ class Inventories extends CI_Model {
     // populate it.
     function create()
     {
-        $names = ['id','name','description','price','picture','category'];
+        $names = ['id','name','quantity'];
         $object = new StdClass;
         foreach ($names as $name)
             $object->$name = "";
@@ -90,7 +90,7 @@ class Inventories extends CI_Model {
     {
             $this->rest->initialize(array('server' => REST_SERVER));
             $this->rest->option(CURLOPT_PORT, REST_PORT);
-            return $this->rest->delete('/maintenance/item/id/' . $key);
+            return $this->rest->delete('inventory/maintenance/item/id/' . $key);
     }
     
     // Determine if a key exists
@@ -98,23 +98,24 @@ class Inventories extends CI_Model {
     {
             $this->rest->initialize(array('server' => REST_SERVER));
             $this->rest->option(CURLOPT_PORT, REST_PORT);
-            $result = $this->rest->get('/maintenance/item/id/' . $key);
+            $result = $this->rest->get('inventory/maintenance/item/id/' . $key);
             return ! empty($result);
     }
     
     // Update a record in the DB
     function update($record)
     {
-            $this->rest->initialize(array('server' => REST_SERVER));
-            $this->rest->option(CURLOPT_PORT, REST_PORT);
-            $retrieved = $this->rest->put('/maintenance/item/id/' . $record['code'], $record);
+        $data = get_object_vars($record);
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $retrieved = $this->rest->put('inventory/maintenance/item/id/' . $data['id'], $data);
     }
     
     // Add a record to the DB
     function add($record)
     {
-            $this->rest->initialize(array('server' => REST_SERVER));
-            $this->rest->option(CURLOPT_PORT, REST_PORT);
-            $retrieved = $this->rest->post('/maintenance/item/id/' . $record['code'], $record);
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $retrieved = $this->rest->post('inventory/maintenance/item/id/' . $record['id'], $record);
     }
 }
